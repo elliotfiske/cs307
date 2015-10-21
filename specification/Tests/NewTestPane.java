63fdf830f119d191068a52ecc929dfb4855e193c
aadrianc <aadrianc@963ccdc2-257a-457c-a1c1-497300e6edd4>
2015-10-21 18:21:20 +0000

    Added NewTestPane.java
    git-svn-id: svn+ssh://unix3.csc.calpoly.edu/home/crhoads/projects/SVN/testtool/trunk@154 963ccdc2-257a-457c-a1c1-497300e6edd4

---
+++ specification/Tests/NewTestPane.java
@@ -0,0 +1,25 @@
+package Tests;
+/**
+ * The initial pane for making a test, lets the teach set basic
+ * information such as time limit, name, course, etc.
+ */
+public abstract class NewTestPane extends RightPane {
+
+   String title;
+   String course;
+   int timeLimit;
+   String description;
+
+   /**
+    * adds the test to the test list and opens the basic generator
+    */ 
+   public abstract void generateTest();
+   /**
+    * Cancels the generation and closes the pane
+    */ 
+   public abstract void cancelGenerateTest();
+   /**
+    * Updates the currTest info
+    */ 
+   public abstract void updateTestInfo();
+}
